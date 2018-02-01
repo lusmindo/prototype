75ca9e48ab123a92bd91953ac1b1ce823dd10d4d
lusmindo <36025430+lusmindo@users.noreply.github.com>
2018-02-01 13:38:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<img src="a.PNG">
+<img src="b.PNG">
+<img src="c.PNG">
+<img src="d.PNG">
+<img src="e.PNG">
+<img src="f.PNG">
+<img src="g.PNG">
+
+</body>
+</html>
